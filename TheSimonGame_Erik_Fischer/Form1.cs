91d35f8bf3c4c5e01c605ca4287f9db574a05fdd
0323fier <0323fier@DESKTOP-FTMV1ID>
2022-04-25 13:10:32 +0200

    Add project files.

---
+++ TheSimonGame_Erik_Fischer/Form1.cs
@@ -0,0 +1,10 @@
+namespace TheSimonGame_Erik_Fischer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TheSimonGame_Erik_Fischer/Program.cs
@@ -0,0 +1,15 @@
+namespace TheSimonGame_Erik_Fischer
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
